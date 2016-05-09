ad2a8664832a06fe1f889ce6caf1c31d27903bab
Kinnal Patel <pkinnal@gmail.com>
2016-05-09 21:48:52 +0530

    Created HMS

---
+++ HotelManagmentSystem/HotelManagmentSystem/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using HotelManagmentSystem.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HotelManagmentSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [CustomAuthorize(Roles = "Admin,TableManager")]
+        public ActionResult Table()
+        {
+            return View();
+        }
+
+        [CustomAuthorize(Roles = "Admin,KitchenManager")]
+        public ActionResult Kitchen()
+        {
+            return View();
+        }
+
+        [CustomAuthorize(Roles = "Admin,CashManager")]
+        public ActionResult Cash()
+        {
+            return View();
+        }
+
+        [CustomAuthorize(Roles = "Admin,TableManager,KitchenManager,CashManager")]
+        public ActionResult UnAuthorize()
+        {
+            return View();
+        }
+
+       
+
+
+    }
+}
---
+++ HotelManagmentSystem/HotelManagmentSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HotelManagmentSystem.Startup))]
+namespace HotelManagmentSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
